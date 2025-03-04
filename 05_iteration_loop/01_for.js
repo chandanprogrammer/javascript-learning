892c940cffafe35e952ec42912d4a48875563766
chandan <chandankumar070501@gmail.com>
2025-03-05 00:01:11 +0530

    add: for and while loop

--- 05_iteration_loop/01_for.js
+++ 05_iteration_loop/01_for.js
@@ -1,43 +1,44 @@
-// loops are use to perform repeated actions
+/* -------- Loops: (iteration) -------- */
 
-// for loop
+/* loops are use to perform repeated actions */
+
+/* for loop --> If you know how many times */
 
 for (let index = 0; index < 10; index++) {
-    let element = index;
-    // console.log(element);
+  console.log(index);
 }
 
 for (let i = 0; i < 5; i++) {
-    // condition check
-    if (i == 4) {
-        // console.log('4 is best number');
-    }
-    // console.log(i);
+  /* condition check */
+  if (i == 4) {
+    // console.log('4 is best number');
+  }
+  // console.log(i);
 }
 
 for (let i = 1; i <= 10; i++) {
-    // console.log(`Outer loop value: ${i}`);
-    for (let j = 1; j <= 10; j++) {
-        // console.log(`Inner loop value ${j} and outer loop ${i}`);
-        // console.log(i + '*' + j + " = " + i * j);
-    }
+  // console.log(`Outer loop value: ${i}`);
+  for (let j = 1; j <= 10; j++) {
+    // console.log(`Inner loop value ${j} and outer loop ${i}`);
+    // console.log(i + '*' + j + " = " + i * j);
+  }
 }
 
 let myArray = ["flash", "batman", "superman"];
 // console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    // console.log(element);
+  const element = myArray[index];
+  // console.log(element);
 }
 
-// break and continue
+/* break and continue */
 
 for (let i = 1; i <= 20; i++) {
-    // if(i==5){
-    //     break;
-    // }
-    if (i == 5) {
-        continue;
-    }
-    console.log(i);
+  // if(i==5){
+  //     break;
+  // }
+  if (i == 5) {
+    continue;
+  }
+  console.log(i);
 }
--- 05_iteration_loop/02_while.js
+++ 05_iteration_loop/02_while.js
@@ -1,21 +1,26 @@
-// while loop
+/* -------- Loops: (iteration) -------- */
 
-let i = 1;
-while (i <= 10) {
-    console.log(i);
-    i++;
+/* while loop --> If you can not know how many times */
+
+let step = 0;
+let house = 100;
+
+while (step <= house) {
+  console.log("Step Taken", step);
+  step++;
 }
 
-// do-while loop
+/* do-while loop --> If you can not know how many times */
 
 let j = 1;
 do {
-    console.log(j);
-    j++;
+  console.log(j);
+  j++;
 } while (j <= 10);
 
-// In case of while loop first step condition check then exicute code but
-// in case of do-while loop first step exicute code then condition check
+/*
+    In case of while loop first step condition check then exicute code but
+    in case of do-while loop first step exicute code then condition check
 
-
-// If the condition never becomes false, the loop will never end and this might crash the runtime.
+    If the condition never becomes false, the loop will never end and this might crash the runtime.
+*/
