4ef09bcd024960cbcfb8fc55cc2c56830cab336e
chandan <chandankumar070501@gmail.com>
2025-03-07 17:09:35 +0530

    add: function, arrow function, iife

--- 08_functions/01_functions.js
+++ 08_functions/01_functions.js
@@ -1,52 +1,85 @@
+/* -------- Create Function -------- */
 
-// Create Function: myFun() ;
+/* 
+    A function is a set of instructions that can be called independently, while a method is a function that's associated with an object or class 
+*/
 
-function myFun(){ // Function definition
-    console.log("C");
-    console.log("h");
-    console.log("a");
-    console.log("n");
-    console.log("d");
-    console.log("a");
-    console.log("n");
+/* Function definition */
+function myFun() {
+  console.log("Chandan");
 }
 // myFun; // Function reference
 // myFun(); // Function execution , Function calling
 
-// function addTwoNumbers(num1, num2){ // parameter
-    // const sum = num1 + num2;
-    // console.log(sum);
-// }
+/* value pass as a 'parameter' */
+function addTwoNumbers(num1, num2) {
+  const sum = num1 + num2;
+  console.log(sum);
+}
 
-// addTwoNumbers(2, 5);
+// addTwoNumbers(2, 5); /* value pass as a 'argument' */
 // addTwoNumbers(2, "5");
 // addTwoNumbers(2, null);
-// addTwoNumbers(2, "a"); // argument
+// addTwoNumbers(2, "a");
 
 // const returnfun  = addTwoNumbers(1, 2);
-// console.log(returnfun); // undefined because no return
+// console.log(returnfun); // undefined because nothing return
 
-function addTwoNumbers(num1, num2){ 
-    // const sum = num1 + num2;
-    // return sum;
-    return num1 + num2
-    // console.log('Unreachable code'); // use before return
+function sumTwoNumbers(num1, num2) {
+  // const sum = num1 + num2;
+  // return sum;
+  return num1 + num2;
+  // console.log('Unreachable code'); // use before return
 }
-const returnfun  = addTwoNumbers(1, 2);
-// console.log(returnfun);
-
+const returnValue = sumTwoNumbers(1, 2);
+// console.log(returnValue);
 
 // function loginUserMessage(username = "defalutValue"){ // defult value
 //     return `${username} just logged in`
 // }
 
-function loginUserMessage(username){
-    if(username === undefined){
-        console.log("plese enter user name");
-        return 
-    }
-    return `${username} just logged in`
+function loginUserMessage(username) {
+  if (username === undefined) {
+    console.log("plese enter user name");
+    return;
+  }
+  return `${username} just logged in`;
 }
 // loginUserMessage("chandan");
 // loginUserMessage();
 // console.log(loginUserMessage("chandan"));
+
+let globalVariable = "I am global variable";
+
+function modifyGlobal() {
+  globalVariable = "I am modified.";
+  let blockScopeVariable = "I am blocked-scoped";
+  console.log(blockScopeVariable);
+}
+
+// console.log(globalVariable);
+// modifyGlobal();
+// console.log(globalVariable);
+
+const user = {
+  username: "chandan",
+  price: 999,
+  welcomeMessage: function () {
+    /* Current contex ke liye "this" keyword use krte hai */
+    console.log(`${this.username} , welcome to website. `);
+    console.log(this);
+  },
+};
+
+// user.welcomeMessage();
+
+// console.log(this); /* --> {} empty object in node environment but in browser global object (known as window object) show */
+
+function person1() {
+  let username = "chandan";
+  console.log(this.username); // undefined
+  /* undefined -- Not use "this" keyword, only use "this" keyword in object */
+  console.log(this);
+  /* When 'this' access inside function then global object show */
+}
+// person1();
--- 08_functions/05_iife.js
+++ 08_functions/05_iife.js
@@ -1,22 +1,23 @@
-// Immediately Invoked Function Expressions (IIFE)
+/* ------ Immediately Invoked Function Expressions (IIFE) ------ */
 
 // (Function defination)(Function execution)
 
-// Named iife
-(function cahi() { 
-    console.log(`DB connected`);
+/* Named iife */
+(function cahi() {
+  console.log(`DB connected`);
 })();
 
 // ; jruri hai iife ke case me
 
-// **Global scope se pollution se problem ke resion se globle scope me jo variable use hota hai us pollution ko htane ke liye iife ka use krte hai 
+/* 
+    Global scope se pollution se problem ke resion se globle scope me jo variable use hota hai us pollution ko htane ke liye iife ka use krte hai.
+*/
 
-// unnamed or normal iife
-(() => { 
-    console.log(`Again db connected`);
+/*  unnamed or normal iife */
+(() => {
+  console.log(`Again db connected`);
 })();
 
-
 ((name) => {
-    console.log(`Again 2 db connected ${name}`);
-})('demo');
+  console.log(`Again 2 db connected ${name}`);
+})("demo");
